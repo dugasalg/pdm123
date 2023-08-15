d9650736a269c226194cf00c88f2e57af7d6e3f8
dugasalg <73446031+dugasalg@users.noreply.github.com>
2023-08-14 19:41:25 -0600

    Comentario de prueba

--- app/src/main/java/com/example/pdm123f/MainActivity.kt
+++ app/src/main/java/com/example/pdm123f/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pdm123f.ui.theme.PDM123fTheme
 
+// Comentario de prueba
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
