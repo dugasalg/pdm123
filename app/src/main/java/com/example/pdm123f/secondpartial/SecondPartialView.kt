82360e0e67eec017190a30733eaf42a9356b6a2b
dugasalg <73446031+dugasalg@users.noreply.github.com>
2023-10-02 20:36:02 -0600

    secparpush

--- app/src/main/java/com/example/pdm123f/secondpartial/SecondPartialView.kt
+++ app/src/main/java/com/example/pdm123f/secondpartial/SecondPartialView.kt
@@ -1,21 +1,34 @@
 package com.example.pdm123f
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
 
 @Composable
-fun SecondPartialView() {
-    Box(
+fun SecondPartialView(navController: NavController) {
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Text(text = stringResource(id = R.string.second_partial))
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        Text(
+            text = stringResource(id = R.string.second_partial)
+        )
+        Button(onClick = { navController.navigate(route = "qr") }) {
+            Text(text = "QR")
+    }
+        Button(onClick = {navController.navigate(route = "listas")}) {
+            Text(text = "Pokemon")
     }
 
+    }
 }
--- app/src/main/java/com/example/pdm123f/secondpartial/onboarding/OnBoardingData.kt
+++ app/src/main/java/com/example/pdm123f/secondpartial/onboarding/OnBoardingData.kt
@@ -1,2 +1,24 @@
 package com.example.pdm123f.secondpartial.onboarding
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+data class OnBoardingData(val title: String)
+
+@Composable
+fun LoaderIntro(modifier: Modifier, image: Int) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = null, // Provide a content description if needed
+        modifier = modifier.fillMaxSize() // Fill the entire available space
+    )
+}
